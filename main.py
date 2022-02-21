51c3c3cfef8473ab21b2a6858cd8553e5bb3fee3
Alessio <galatolo@kth.se>
2022-02-21 13:12:37 +0100

    Testing emotions

---
+++ main.py
@@ -0,0 +1,67 @@
+from furhat_remote_api import FurhatRemoteAPI
+
+# FURHAT_ADDRESS = "MSI.local"
+FURHAT_ADDRESS = "localhost"
+
+# Create an instance of the FurhatRemoteAPI class, providing the address of the robot or the SDK running the virtual robot
+furhat = FurhatRemoteAPI(FURHAT_ADDRESS)
+
+# Get the voices on the robot
+voices = furhat.get_voices()
+
+# Set the voice of the robot
+furhat.set_voice(name='Matthew')
+
+# Say "Hi there!"
+furhat.say(text="Hi there!")
+
+# Play an audio file (with lipsync automatically added)
+furhat.say(url="https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav", lipsync=True)
+
+# Listen to user speech and return ASR result
+# result = furhat.listen()
+
+# Perform a named gesture
+furhat.gesture(name="BrowRaise")
+#EXPR_ANGER
+    # EXPR_DISGUST
+    # EXPR_FEAR
+    # EXPR_SAD
+
+# Perform a custom gesture
+furhat.gesture(body={
+    "frames": [
+        {
+            "time": [
+                0.33
+            ],
+            "params": {
+                "BLINK_LEFT": 1.0
+            }
+        },
+        {
+            "time": [
+                0.67
+            ],
+            "params": {
+                "reset": True
+            }
+        }
+    ],
+    "class": "furhatos.gestures.Gesture"
+    })
+
+# Get the users detected by the robot
+users = furhat.get_users()
+
+# Attend the user closest to the robot
+furhat.attend(user="CLOSEST")
+
+# Attend a user with a specific id
+furhat.attend(userid="virtual-user-1")
+
+# Attend a specific location (x,y,z)
+furhat.attend(location="0.0,0.2,1.0")
+
+# Set the LED lights
+furhat.set_led(red=50, green=200, blue=50)
