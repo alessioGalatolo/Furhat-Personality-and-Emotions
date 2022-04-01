ef1307ecd5cbd9c313d9d14702b0366f94811aae
Alessio <galatolo@kth.se>
2022-04-01 10:11:36 +0200

    Reuse dataset functions

--- personality_transfer_model/model_wrapper.py
+++ personality_transfer_model/model_wrapper.py
@@ -3,6 +3,7 @@ import os.path as path
 import texar.torch as tx
 import torch
 from ctrl_gen_model import CtrlGenModel
+from personality_transfer_model.dataset import TextDataset
 
 
 class PersonalityTransfer:
@@ -18,10 +19,7 @@ class PersonalityTransfer:
             exit(-1)
         self.input_len = checkpoint['input_len']
 
-        # FIXME: should not access that default hparams
-        vocab_hp = tx.HParams({'vocab_file': f"personality_transfer_model/data/{checkpoint['dataset']}/vocab"},
-                              tx.data.data.multi_aligned_data._default_dataset_hparams())
-        self.vocab = tx.data.MultiAlignedData.make_vocab([vocab_hp])[0]
+        self.vocab = TextDataset.load_vocab(f"personality_transfer_model/data/{checkpoint['dataset']}/vocab")
         self.model = CtrlGenModel(self.input_len,
                                   self.vocab, config, device)
         self.model.load_state_dict(checkpoint['model_state_dict'], strict=True)
@@ -30,12 +28,7 @@ class PersonalityTransfer:
         # FIXME what if the text is not in the vocab?
         # Eval
         text_tokens = text.split()
-        text_tokens.insert(0, self.vocab.bos_token)
-        text_tokens.append(self.vocab.eos_token)
-        if len(text_tokens) < self.input_len:  # FIXME: check length and eventually pad sequence
-            for _ in range(self.input_len-len(text_tokens)):
-                text_tokens.append('')
-        text_ids = self.vocab.map_tokens_to_ids_py(text_tokens)
+        text_ids = TextDataset.text2ids(text_tokens, self.vocab, self.input_len)
         output_ids = self.model.infer(text_ids, transfer_clas)
 
         hyps = self.vocab.map_ids_to_tokens_py(output_ids)
@@ -45,7 +38,7 @@ class PersonalityTransfer:
 
 # Testing
 if __name__ == "__main__":
-    personality_transfer = PersonalityTransfer('config')
-    original_text = "I have the feeling that this thing doesn't work at all ."
+    personality_transfer = PersonalityTransfer('config', checkpoint_name='ckpt_epoch_6.pth')
+    original_text = "I m not even sure if this thing works better than the other ."
     print(f'Original text: {original_text}')
     print(f'Transferred text: {personality_transfer.transfer(original_text)}')
