ed3223e6da0567c96868b18188c6d7381ca0f4b9
Alessio <galatolo@kth.se>
2022-05-12 10:56:27 +0200

    Add pilot questions, split fluency measures

---
+++ pilot_data/study_questions.py
@@ -0,0 +1,29 @@
+# Map between question number and question text
+# Questions shown on a 7-point Likert scale
+questions = {
+    1: "is introverted",
+    2: "is reserved",
+    3: "tends to be quiet",
+    4: "is sometimes shy, inhibited",
+    5: "holds back their opinions",
+    6: "is extroverted",
+    7: "is talkative",
+    8: "is full of energy",
+    9: "generates a lot of enthusiasm",
+    10: "is outgoing, sociable",
+    11: "is fluent in english",
+    12: "does not make grammatical errors",
+    13: "is coherent and consistent with what they say",
+    14: "click 'Strongly disagree' for this question"  # attention check
+}
+
+# Map between attention check number and attention check text
+# Attention checks were shown with checkboxs
+attention_check = {
+    0: "An introduction of a companion robot",  # Correct one
+    1: "A talk on humanities",  # Correct one
+    2: "A conversation between a student and their supervisor",
+    3: "A presentation of a vaccum cleaner",
+    4: "A talk on STEM subjects",
+    5: "A dialogue between two people working together"  # Correct one
+}
